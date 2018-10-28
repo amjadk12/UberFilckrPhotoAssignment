5315465b13495ab0f70bf57e5b754f720c98e3e3
Amjad Khan <amjadk12@gmail.com>
2018-10-28 19:47:11 +0530

    Updated the Styles

--- Component/Header.js
+++ Component/Header.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import { StyleSheet, Image, View, Text,TextInput,TouchableOpacity } from "react-native";
-import "./App.css";
-
- 
 
 export class Header extends Component {
   constructor(props){
@@ -21,21 +18,52 @@ export class Header extends Component {
   render() {
     let { onNewImageElement, onUpdate } = this.props;
     return (
-      <View className=".header">
+      <View style={styles.header}>
       <TextInput
-        className=".searchInput"
+        style={styles.searchInput}
         onChangeText={this.update}
         value={this.state.textSearch}
       />
-         <TouchableOpacity  className=".button" onPress={onNewImageElement}>
+         <TouchableOpacity style={styles.button} onPress={onNewImageElement}>
            <Image
           source={require("../assets/search.png")}
-          className=".searchControl"
+          style={styles.searchControl}
         />
         </TouchableOpacity>
       </View>
     );
   }
 }
-
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    marginBottom: 20,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    height: "10%",
+    marginTop: 10,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alighnItem: "center",
+    justifyContent: "center",
+    padding: 5,
+    borderBottomWidth: 10,
+    borderBottomColor: "#ccc"
+  },
+  searchControl: {
+    width: 25,
+    height: 25
+  },
+  searchInput: {
+    fontStyle: "italic",
+    height: 30,
+    width: "50%", 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    marginBottom: 20,
+    padding:7
+  }
+});
 export default Header;
